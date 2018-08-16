2c24da68891e740f808c40dd83f5c8a61d8847be
Raquel117 <42181871+Raquel117@users.noreply.github.com>
2018-08-16 12:08:24 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+#Autor: Raquel Collado Islas
+#Fecha:16 Ago. 2018
+#
+s=12
+p=input("Numero p: ")
+while s!=0:
+ else:
+  p=p+2
+    if p%2=0:
+     p=p+1
+    else:
+      while a<p-1:
+      a=a+1
+      r=p%a
+       if r==0:
+        s=s-p
+       else:
+        s=s-1
+print(p)
