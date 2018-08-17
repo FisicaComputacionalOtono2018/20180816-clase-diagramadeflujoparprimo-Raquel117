b2b019a23168bba36bd979b534025775a3d740eb
Raquel117 <42181871+Raquel117@users.noreply.github.com>
2018-08-17 11:58:31 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 #Autor: Raquel Collado Islas
-#Fecha:16 Ago. 2018
+#Fecha:17 Ago. 2018
 #
-s=12
+
+def Primo(x):
+   if x<2:
+      flag=False
+   elif p==2:
+      flag=True
+   else:
+      flag=True
+      for n in range (2,x-1):
+         if x%n==0:
+            flag= False
+            break
+   return flag
+
 p=input("Numero p: ")
-while s!=0:
- else:
-  p=p+2
-    if p%2=0:
-     p=p+1
-    else:
-      while a<p-1:
-      a=a+1
-      r=p%a
-       if r==0:
-        s=s-p
-       else:
-        s=s-1
-print(p)
+if Primo(p):
+   print("Es primo")
+else:
+   print("No es primo")
+
